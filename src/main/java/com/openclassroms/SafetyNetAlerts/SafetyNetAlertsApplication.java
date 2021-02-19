6837c924f3b9bfe041f5036bf7a31b515f41d853
christine <christine.zerrouk@gmail.com>
2021-02-19 11:15:43 +0100

    Mise à jour de la classe d'appel principale

--- src/main/java/com/openclassroms/SafetyNetAlerts/SafetyNetAlertsApplication.java
+++ src/main/java/com/openclassroms/SafetyNetAlerts/SafetyNetAlertsApplication.java
@@ -1,7 +1,27 @@
 package com.openclassroms.SafetyNetAlerts;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.openclassroms.SafetyNetAlerts.model.Personn;
+import com.openclassroms.SafetyNetAlerts.service.PersonService;
+import com.openclassroms.SafetyNetAlerts.util.ReadJsonForPersons;
+import com.openclassroms.SafetyNetAlerts.util.ReadJsonForFireStations;
+import org.apache.tomcat.util.json.JSONParser;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.context.annotation.Bean;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 public class SafetyNetAlertsApplication {
@@ -11,3 +31,5 @@ public class SafetyNetAlertsApplication {
 	}
 
 }
+
+
