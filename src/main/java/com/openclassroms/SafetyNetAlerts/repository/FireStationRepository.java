11c39f314cc56b10f54f3c3179e3b70b36c66dd5
christine <christine.zerrouk@gmail.com>
2021-02-19 11:09:33 +0100

    Création du repository FireStation.java

---
+++ src/main/java/com/openclassroms/SafetyNetAlerts/repository/FireStationRepository.java
@@ -0,0 +1,7 @@
+package com.openclassroms.SafetyNetAlerts.repository;
+
+import com.openclassroms.SafetyNetAlerts.model.FireStation;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FireStationRepository extends CrudRepository<FireStation,String> {
+}
